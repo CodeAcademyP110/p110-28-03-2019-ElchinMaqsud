be760cc1db5a89c7b9249e825dabfba17312303c
ElchinMaqsud <host9797@mail.ru>
2019-04-01 15:11:34 +0400

    Homework 28.03.2019

---
+++ js/main.js
@@ -0,0 +1,196 @@
+
+"use strict"
+
+
+// START OF HOMETASK (1) //
+
+// function spaceindex() {
+//     let userInput = prompt("Please enter a sentence");
+
+//     let userSentence = userInput;
+//     userSentence = userInput.split(" ");
+//     let string = userSentence.length - 1
+//     return string;
+// }
+// console.log("Boslugun sayi: " + spaceindex())
+
+// END OF HOMETASK (1) //
+
+
+
+
+
+// START OF HOMETASK (2) //
+
+// let array = [];
+// let userInput = +prompt("Reqemlerden ibaret massivi daxil edin!!")
+// while (isNaN(userInput) || userInput === 0) {
+//     userInput = +prompt(" Yalniz Reqemlerden ibaret massivi daxil edin!!")
+// }
+// let userNumber = userInput;
+// array.push(userNumber)
+
+// console.log(array)
+
+// function numericalAvg(arr) {
+//     let sum = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//         if(+arr[i]){
+//             sum = sum + arr[i];
+//         }
+
+
+//     }
+//     sum/=arr.length
+//     return sum
+// }
+//  numericalAvg()
+
+// END OF HOMETASK (2) //
+
+
+
+
+
+
+// START OF HOMETASK (3) //
+
+// function Evens(arr){
+//     let evenNumber=[];
+//     for(let i=0; i<arr.length;i++){
+//         if(arr[i]%2==0){
+//             evenNumber.push(arr[i])
+//         }
+//     }
+//     return evenNumber
+// }
+// console.log(Evens())
+
+// END OF HOMETASK (3) //
+
+
+
+
+
+
+// START OF HOMETASK (4) //
+// function myNumber(arr){
+//     let sum=0;
+//     let elders=[]
+//     for(let i=0;i<arr.length;i++){
+
+//         sum+=arr[i]
+
+//     }
+//     sum/=arr.length
+
+
+//     for( let i=0;i<arr.length;i++){
+
+//         if(arr[i]>sum){
+//             elders.push(arr[i])
+//         }
+
+//     }
+//     return elders
+
+// }
+// console.log(myNumber())
+
+// END OF HOMETASK (4) //
+
+
+
+
+
+
+
+// START OF HOMETASK (5) //
+
+
+// function United(par){
+
+//     let result="";
+//     for (let index = 0; index < par.length; index++) {
+//         if (index == par.length-1) {
+//             result += par[index];
+//         }
+//         else{
+//             result += par[index] + ", ";
+//         }
+//     }
+
+//     return result;
+// }
+// console.log(United())
+
+
+// END OF HOMETASK (5) //
+
+
+
+
+
+
+
+//  START OF HOMETASK(6)  ////
+
+// function reverseArr(arr) {
+//     let reverseArray = [];
+//     for (let i = arr.length - 1; i >= 0; i--) {
+//         reverseArray.push(arr[i])
+//     }
+//     return reverseArray;
+// }
+
+
+// console.log(reverseArr())
+
+
+// END OF HOMETASK (6) //
+
+
+
+
+
+
+// START OF HOMETASK (7) //
+
+// function equalNumber(arr1, arr2) {
+//     let equalarray = [];
+
+//     for (let i = 0; i < arr1.length; i++) {
+//         if (arr1.indexOf(arr2[i]) != -1) {
+
+//             equalarray.push(arr2[i])
+//         }
+//     }
+//     return equalarray
+// }
+
+// console.log(equalNumber())
+
+
+// END OF HOMETASK (7) //
+
+
+
+
+
+
+//  START OF HOMETASK 8 //
+// function same(arr,par){
+//     let array2=[]
+//     for(let i=0; i<arr.length;i++){
+//         if(arr[i][0]==par){
+//             array2.push(arr[i])
+//         }
+//     }
+//     return array2
+// }
+
+// console.log(same())
+
+// END OF HOMETASK 8 
+
+
